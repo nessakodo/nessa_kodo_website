988b67b4a1acc0b50bb55cbf8cabf7080818af85
Nessa <nessakodo@gmail.com>
2022-07-29 15:31:04 -0600

    Updated button function and position, fixed image sizing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement("h2");
+/* const h2 = document.createElement("h2");
 h2.textContent = "ABOUT ME";
 
 document.querySelector("body").appendChild(h2);
@@ -7,9 +7,12 @@ const h3 = document.createElement("h3");
 h3.textContent = "My love for technology was reignited when I entered the NFT space in early 2021. Since then, I have been actively teaching myself new mediums, softwares, and methods to help me integrate my current skillsets into the more useful applications within the tech sphere. I am currently working to learn Software Engineering at the Flatiron School, in Denver, Colorado.";
 
 document.querySelector("body").appendChild(h3);
+*/
+
+const input = document.getElementById("button");
 
 function myFunction() {
   alert ("Whenever you find yourself doubting how far you can go, just remember how far you have come.");
 }
 
-document.getElementById("button").addEventListener("click", myFunction);
+input.addEventListener('click', myFunction);
