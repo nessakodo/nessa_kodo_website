1bc906f57ddf47237a3dad4ef7cdba3f7faa861a
Nessa <nessakodo@gmail.com>
2022-07-29 15:43:44 -0600

    More fixes and sizing changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* 
+
 const h2 = document.createElement("h2");
 h2.textContent = "ABOUT ME";
 
@@ -6,7 +8,7 @@ document.querySelector("body").appendChild(h2);
 const h3 = document.createElement("h3");
 h3.textContent = "My love for technology was reignited when I entered the NFT space in early 2021. Since then, I have been actively teaching myself new mediums, softwares, and methods to help me integrate my current skillsets into the more useful applications within the tech sphere. I am currently working to learn Software Engineering at the Flatiron School, in Denver, Colorado.";
 
-document.querySelector("body").appendChild(h3);
+document.querySelector("body").appendChild(h3); */
 
 const input = document.getElementById("button");
 
